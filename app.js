e6a1e1c05dfea79ecd16d2c4e7e8abc9d28e8f88
Darren Skidmore <45662167+drskidmore@users.noreply.github.com>
2022-12-22 12:05:05 -0700

    Added global leaderboard, event history, and handling for archived events

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const fs = require('fs')
 const axios = require('axios')
+const db = require('./db')
+const { cached } = require('sqlite3')
 
 const app = express()
 const port = 3000
@@ -14,108 +16,244 @@ fs.readFile(__dirname + '/hh-config.json', 'utf8', (err, data) => {
   } else {
     console.log('Successfully loaded API config.')
     hhcfg = JSON.parse(data)
-    hhcfg["fullBase"] = hhcfg["base"] + '/project/' + hhcfg["application"]
+    hhcfg["fullBaseLeaderboard"] = hhcfg["baseLeaderboard"] + '/project/' + hhcfg["application"]
+    hhcfg["fullBasePlayerMeta"] = hhcfg["basePlayerMeta"] + '/project/' + hhcfg["application"]
   }
 })
 
 // load static content
 app.use(express.static('public'))
-app.use(express.static(__dirname + "/node_modules/bootstrap/dist/"))
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/'))
 
-// master player API
-app.get('/api/event/now/:id', async (req, res) => {
-  const id = req.params.id
-  console.log(`Querying player ${id}`)
-
-  let getCurrentEventId = async() => {
-    const eventListReq = await axios.get(`${hhcfg["fullBase"]}/leaderboards`, config={
+// returns a list of every event since the inception of v3 leaderboard service up until now
+const getAllEvents = async () => {
+  const eventList = []
+  const eventListReq = await axios.get(`${hhcfg["fullBaseLeaderboard"]}/leaderboards`, config={
     "headers": {
+      "_token": hhcfg["token"],
+      "User-agent": "Axios"
+    }
+  })
+
+  // iterate through all leaderboards exposed by the API
+  for (let i of eventListReq["data"]["data"]) {
+    // some "template" events should be ignored
+    if (i["instance"]["definition"]["requirements"].length === 0) {
+      continue
+    }
+
+    const eventId = i["instance"]["guid"]
+    const eventName = i["instance"]["definition"]["name"]
+    const startDate = new Date(i["instance"]["definition"]["requirements"][0]["dateFrom"])
+    const endDate = new Date(i["instance"]["definition"]["requirements"][0]["dateTo"])
+    const currentDate = new Date()
+
+    if (i["instance"]["definition"]["project"] === 'adcom' && (endDate < currentDate || (endDate > currentDate && startDate < currentDate))) {
+      // found a valid event
+      const eventStruct = {
+        "eventId": eventId,
+        "eventName": eventName,
+        "startDate": startDate,
+        "endDate": endDate
+      }
+      eventList.push(eventStruct)
+    }
+  }
+
+  // sort by date bounds
+  eventList.sort((a, b) => b["startDate"] - a["startDate"])
+  return eventList
+}
+
+// returns a bool indicating if the PlayFab ID exists
+const getPlayerState = async(id) => {
+  const dbHandler = new db()
+  const cachedPlayerState = await dbHandler.getPlayer(id)
+
+  if (cachedPlayerState) {
+    // check cache first
+    return true
+  }
+
+  try {
+    await axios.get(`${hhcfg["fullBasePlayerMeta"]}/players/${id}`, config={
+      "headers": {
         "_token": hhcfg["token"],
         "User-agent": "Axios"
       }
     })
 
-    for (let i of eventListReq["data"]["data"]) {
-      // some "template" events should be ignored
-      if (i["instance"]["definition"]["requirements"].length === 0) {
-        continue
-      }
-      const startDate = new Date(i["instance"]["definition"]["requirements"][0]["dateFrom"])
-      const endDate = new Date(i["instance"]["definition"]["requirements"][0]["dateTo"])
-      const currentDate = new Date()
+    // add player to cache
+    dbHandler.addPlayer(id)
+    return true
+  } catch (e) {
+    return false
+  } finally {
+    // close db connection
+    dbHandler.close()
+  }
+}
 
-      if (currentDate < endDate && currentDate >= startDate && i["instance"]["definition"]["project"] === 'adcom') {
-        // found an event in the active date bounds
-        return i["instance"]["guid"]
+// returns a bool indicating if the PlayFab ID participated in the event
+const getPlayerEventState = async(id, eventId, isCurrent) => {
+  const dbHandler = new db()
+  const cachedPlayerEventState = await dbHandler.getPlayerEvents(id)
+
+  // check cache first
+  for (let i of cachedPlayerEventState) {
+    if (i["eventId"] == eventId) {
+      return Boolean(i["exists"])
+    }
+  }
+
+  const playerEventRecord = await axios.get(`${hhcfg["fullBaseLeaderboard"]}/leaderboards/${eventId}/players/${id}`, config={
+    "headers": {
+      "_token": hhcfg["token"],
+      "User-agent": "Axios"
+    }
+  })
+
+  if (playerEventRecord["data"]["data"]) {
+    // player has participated in event
+
+    dbHandler.addPlayerEvent(id, eventId, true)
+    dbHandler.close()
+
+    return true
+  } else {
+    // player has NOT participated in event
+
+    if (!isCurrent) {
+      // they could join later if it's the current event, so only flag if it's passed
+      dbHandler.addPlayerEvent(id, eventId, false)
+      dbHandler.close()
+    }
+
+    return false
+  }
+}
+
+// returns a fully updated synopsis of player participation
+const getKnownPlayerEvents = async(id) => {
+  const dbHandler = new db()
+  const allEvents = await getAllEvents()
+  const currentKnownEvents = await dbHandler.getPlayerEvents(id)
+  const newKnownEvents = []
+
+  // inefficient O(mn) loop but luckily this isn't a lot of data
+  for (let i of allEvents) {
+    let foundIt = false
+    for (let j of currentKnownEvents) {
+      if (i["eventId"] === j["eventId"]) {
+        // found a candidate match, so we know it's included
+        foundIt = true
       }
     }
     
-    // no event found
-    return null
-  }
-
-  let getPlayerEventInfo = async(eventId) => {
-    try {
-      const playerEventReq = await axios.get(`${hhcfg["fullBase"]}/leaderboards/${eventId}/players/${id}`, config={
-        "headers": {
-          "_token": hhcfg["token"],
-          "User-agent": "Axios"
-        }
-      })
-      
-      return playerEventReq["data"]["data"]
-    } catch (e) {
-      if (e.response.status !== 404) {
-        console.error(e.response.status)
-      } else {
-        console.log(`Player ${id} not found`)
-      }
-
-      return null
+    const isCurrent = Boolean(i["eventId"] == allEvents[0]["eventId"])
+    const status = await getPlayerEventState(id, i["eventId"], isCurrent)
+    const eventStruct = {
+      "eventId": i["eventId"],
+      "eventName": i["eventName"],
+      "startDate": i["startDate"],
+      "endDate": i["endDate"],
+      "status": status
     }
+
+    newKnownEvents.push(eventStruct)
   }
 
-  let getPlayerLeaderboard = async(eventId) => {
-    try {
-      // todo: add ability to adjust adjacent players (offset) and top players (topCount)
-      const playerLeaderboardReq = await axios.get(`${hhcfg["fullBase"]}/leaderboards/${eventId}/players/${id}/results?offset=25&resolvePlayers=true&topCount=25`, config={
-        "headers": {
-          "_token": hhcfg["token"],
-          "User-agent": "Axios"
-        }
-      })
-      
-      return playerLeaderboardReq["data"]["resultMap"]
-    } catch (e) {
-      if (e.response.status !== 404) {
-        console.error(e.response.status)
-      } else {
-        console.log(`Player ${id} not found`)
+  newKnownEvents.sort((a, b) => b["startDate"] - a["startDate"])
+  return newKnownEvents
+}
+
+// return information about individual player event registration
+const getPlayerEventInfo = async(id, eventId) => {
+  try {
+    const playerEventReq = await axios.get(`${hhcfg["fullBaseLeaderboard"]}/leaderboards/${eventId}/players/${id}`, config={
+      "headers": {
+        "_token": hhcfg["token"],
+        "User-agent": "Axios"
       }
-
-      return null
-    }
-  }
-
-  const currentEventId = await getCurrentEventId()
-  const playerEventInfo = await getPlayerEventInfo(currentEventId)
-  let returnStruct = {}
-
-  if (playerEventInfo === null) {
-    // don't even bother continuing if we know the ID has no record
-    console.log(`Player ${id} not found`)
-    res.sendStatus(404)
+    })
+    
+    return playerEventReq["data"]["data"]
+  } catch (e) {
+    console.error(e.response.status)
     return
   }
+}
 
-  const playerLeaderboard = await getPlayerLeaderboard(currentEventId)
+// return the player's division leaderboard and global leaderboard
+const getPlayerLeaderboard = async(id, eventId, adjacentCount, topCount) => {
+  try {
+    // todo: add ability to adjust adjacent players (offset) and top players (topCount)
+    const playerLeaderboardReq = await axios.get(`${hhcfg["fullBaseLeaderboard"]}/leaderboards/${eventId}/players/${id}/results?offset=${adjacentCount}&resolvePlayers=true&topCount=${topCount}`, config={
+      "headers": {
+        "_token": hhcfg["token"],
+        "User-agent": "Axios"
+      }
+    })
+    
+    return playerLeaderboardReq["data"]["resultMap"]
+  } catch (e) {
+    console.error(e.response.status)
+    return
+  }
+}
+
+// returns the trophies at an exact position
+const getPosition = async(id, eventId, n) => {
+  try {
+    const positionReq = await axios.get(`${hhcfg["fullBaseLeaderboard"]}/leaderboards/${eventId}/players/${id}/score/position?position=${n}`, config={
+      "headers": {
+        "_token": hhcfg["token"],
+        "User-agent": "Axios"
+      }
+    })
+
+    return positionReq["data"]["resultMap"]["rootResult"]["score"]
+  } catch (e) {
+    console.error(e.response.status)
+    return
+  }
+}
+
+// returns the brackets for a leaderboard
+const getBrackets = async(id, eventId, brackets, totalPlayers) => {
+  const trophies = {}
+
+  for (let i of brackets) {
+    let n = i - 1
+    if (i < 1) {
+      n = Math.floor(totalPlayers * i) - 1
+    }
+    const position = await getPosition(id, eventId, n)
+
+    trophies[i.toString()] = Math.floor(position)
+  }
+
+  return trophies
+}
+
+// returns the data for an active event
+const rankedEntryData = async(currentEventData, playerEventInfo, playerLeaderboard, id) => {
   let proximalPlayerHashMap = {}
   for (let i of playerLeaderboard["resolvedPlayers"]["objectArray"]) {
     // generate a list of players from the "resolvedPlayers" key, so we can obtain metadata such as ordinal ID (yields nickname) and custom name/icon (planned implementation in 2023)
     proximalPlayerHashMap[i["playerId"]] = i["sequence"]
   }
 
-  // is there a more elegant way to approach this?
+  let returnStruct = {}
+
+  returnStruct["status"] = "active"
+
+  returnStruct["event"] = {}
+  returnStruct["event"]["eventGuid"] = currentEventData["eventId"]
+  returnStruct["event"]["eventName"] = currentEventData["eventName"]
+  returnStruct["event"]["dateCommence"] = currentEventData["startDate"]
+  returnStruct["event"]["dateConclude"] = currentEventData["endDate"]
+
   returnStruct["player"] = {}
   returnStruct["player"]["leaderboardGuid"] = playerEventInfo["leaderboardGuid"]
   returnStruct["player"]["playerId"] = playerEventInfo["playerId"]
@@ -130,7 +268,6 @@ app.get('/api/event/now/:id', async (req, res) => {
   returnStruct["global"]["count"] = playerLeaderboard["results"]["rootResults"]["topResults"]["rankedEntry"][0]["position"]["count"]
   returnStruct["global"]["top"] = []
   returnStruct["global"]["adjacent"] = []
-  // todo: add support for old leaderboards (rankedEntry -> archivedEntry)
   for (let i of playerLeaderboard["results"]["rootResults"]["topResults"]["rankedEntry"]) {
     returnStruct["global"]["top"].push({
       "playerId": i["playerId"],
@@ -174,7 +311,148 @@ app.get('/api/event/now/:id', async (req, res) => {
     })
   }
 
-  res.json(returnStruct)
+  return returnStruct
+}
+
+// returns the data for an archived event (which is way less unfortunately)
+const archivedEntryData = async(currentEventData, playerEventInfo, playerLeaderboardInfo) => {
+  let returnStruct = {}
+
+  returnStruct["status"] = "archived"
+
+  returnStruct["event"] = {}
+  returnStruct["event"]["eventGuid"] = currentEventData["eventId"]
+  returnStruct["event"]["eventName"] = currentEventData["eventName"]
+  returnStruct["event"]["dateCommence"] = currentEventData["startDate"]
+  returnStruct["event"]["dateConclude"] = currentEventData["endDate"]
+
+  returnStruct["player"] = {}
+  returnStruct["player"]["leaderboardGuid"] = playerEventInfo["leaderboardGuid"]
+  returnStruct["player"]["playerId"] = playerEventInfo["playerId"]
+  returnStruct["player"]["playerOrdinal"] = -1
+  returnStruct["player"]["divisionRoot"] = playerEventInfo["root"]
+  returnStruct["player"]["divisionId"] = playerEventInfo["segmentId"]
+  returnStruct["player"]["trophies"] = playerEventInfo["score"]
+  returnStruct["player"]["dateJoined"] = playerEventInfo["created"]
+  returnStruct["player"]["dateUpdated"] = playerEventInfo["updated"]
+  returnStruct["player"]["globalPosition"] = playerLeaderboardInfo["results"]["rootResults"]["offsetResults"]["archivedEntry"]["rootPosition"]["position"]
+  
+  returnStruct["global"] = {}
+  returnStruct["global"]["count"] = playerLeaderboardInfo["results"]["rootResults"]["offsetResults"]["archivedEntry"]["rootPosition"]["count"]
+  
+  returnStruct["division"] = {}
+  returnStruct["division"]["count"] = playerLeaderboardInfo["results"]["rootResults"]["offsetResults"]["archivedEntry"]["segmentPosition"]["count"]
+
+  return returnStruct
+}
+
+// get event list
+app.get('/api/list', async (req, res) => {
+  const allEvents = await getAllEvents()
+  res.send(allEvents)
+})
+
+// check if player exists
+app.get('/api/player/:id', async (req, res) => {
+  const id = req.params.id
+  console.log(`Testing player ${id}`)
+
+  const playerState = await getPlayerState(id)
+  if (playerState) {
+    res.status(200).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+// get list of all registered events for a player
+app.get('/api/list/:id', async (req, res) => {
+  const id = req.params.id
+  console.log(`Checking all events for ${id}`)
+
+  const allKnownPlayerEvents = await getKnownPlayerEvents(id)
+  if (allKnownPlayerEvents) {
+    res.status(200).json(allKnownPlayerEvents)
+  } else {
+    res.status(404).end()
+  }
+})
+
+// master player API
+app.get('/api/event/:event/:id', async (req, res) => {
+  const id = req.params.id
+  const eventId = req.params.event
+  console.log(`Querying player ${id} for event ${eventId}`)
+
+  const allEventData = await getAllEvents()
+  let currentEventData
+
+  for (let i of allEventData) {
+    if (i["eventId"] == eventId) {
+      currentEventData = i
+      break
+    }
+  }
+
+  const playerEventInfo = await getPlayerEventInfo(id, eventId)
+  if (!playerEventInfo) {
+    // don't even bother continuing if we know the ID has no record
+    console.log(`Player ${id} not found`)
+    res.status(404).end()
+    return
+  }
+
+  const playerLeaderboard = await getPlayerLeaderboard(id, eventId, 25, 25)
+  if (!playerLeaderboard) {
+    // don't even bother continuing if we know the ID has no record
+    console.log(`Player ${id} not registered with ${eventId}`)
+    res.status(404).end()
+    return
+  }
+
+  // differentiate between archived and active leaderboard
+  let returnStruct
+  if (playerLeaderboard["results"]["rootResults"]["topResults"]) {
+    // active
+    returnStruct = await rankedEntryData(currentEventData, playerEventInfo, playerLeaderboard, id)
+  } else {
+    // archive
+    returnStruct = await archivedEntryData(currentEventData, playerEventInfo, playerLeaderboard)
+  }
+  
+  res.status(200).json(returnStruct)
+})
+
+// brackets
+app.get('/api/event/:event/:id/brackets', async (req, res) => {
+  const id = req.params.id
+  const eventId = req.params.event
+  const brackets = [1, 5, 25, 100, 0.01, 0.05, 0.10, 0.25, 0.50, 0.75]
+  console.log(`Querying player ${id} for event ${eventId} (lb brackets)`)
+
+  const playerLeaderboard = await getPlayerLeaderboard(id, eventId, 25, 25)
+  if (!playerLeaderboard) {
+    // don't even bother continuing if we know the ID has no record
+    console.log(`Player ${id} not registered with ${eventId}`)
+    res.status(404).end()
+    return
+  }
+
+  // hacky way to determine number of players
+  let totalPlayers
+  if (playerLeaderboard["results"]["rootResults"]["offsetResults"]["rankedEntry"]) {
+    totalPlayers = playerLeaderboard["results"]["rootResults"]["offsetResults"]["rankedEntry"][0]["position"]["count"]
+  } else {
+    totalPlayers = playerLeaderboard["results"]["rootResults"]["offsetResults"]["archivedEntry"]["rootPosition"]["count"]
+  }
+
+  const bracketData = await getBrackets(id, eventId, brackets, totalPlayers)
+  const returnStruct = {
+    "totalPlayers": totalPlayers,
+    "brackets": bracketData
+  }
+
+  res.status(200).json(returnStruct)
 })
 
 app.listen(port, () => {
---
+++ public/js/script.js
@@ -0,0 +1,268 @@
+const postFormPlayFab = async() => {
+  const playerId = document.querySelector('#playFabQuery').value
+  const saveCurrentId = document.querySelector('#playFabSetDefault')
+  
+  const userExists = await getPlayerState(playerId)
+  
+  if (userExists) {
+    // success
+    if (saveCurrentId.checked) {
+      localStorage.setItem('playerId', playerId)
+    } else {
+      localStorage.removeItem('playerId')
+    }
+    
+    document.querySelector('#noAccountFound').classList.add('d-none')
+    document.querySelector('#activePlayFabId').innerText = playerId
+
+    // load event list
+    await postFormEventList()
+  } else {
+    // no user found or general failure
+    document.querySelector('#noAccountFound').classList.remove('d-none')
+  }
+}
+
+const postFormEventList = async() => {
+  // disable dropdown while we load, and show "loading" option
+  const selectList = document.querySelector('#eventSelect')
+  const selectButton = document.querySelector('#formSubmitEvent')
+
+  while (selectList.childElementCount > 0) {
+    selectList.remove(selectList.lastChild)
+  }
+
+  const newOption = document.createElement('option')
+  newOption.innerText = 'Please wait ...'
+  selectList.appendChild(newOption)
+  selectList.setAttribute('disabled', 'disabled')
+  selectButton.setAttribute('disabled', 'disabled')
+
+  const playerId = document.querySelector('#activePlayFabId').innerText
+  const eventList = await getPlayerEventList(playerId)
+
+  if (eventList) {
+    selectList.removeAttribute('disabled')
+    selectButton.removeAttribute('disabled')
+
+    const templateOption = document.querySelector('option')
+    templateOption.remove()
+
+    for (let i of eventList) {
+      if (i["status"]) {
+        // the player participated in this event
+        const newOption = document.createElement('option')
+        newOption.innerText = `${i["eventName"]} ${i["startDate"]}`
+        newOption.value = i["eventId"]
+        
+        selectList.appendChild(newOption)
+      }
+    }
+
+    if (selectList.childElementCount < 1) {
+      // if after all this there have been no events found, we will restore the template message
+      // it is possible for a PlayFab account to exist but no events to have been participated in
+      const newOption = document.createElement('option')
+      newOption.innerText = 'No event participation'
+      selectList.appendChild(newOption)
+      selectList.setAttribute('disabled', 'disabled')
+      selectButton.setAttribute('disabled', 'disabled')
+    }
+  }
+}
+
+const postFormEvent = async() => {
+  const playerId = document.querySelector('#activePlayFabId').innerText
+  const selectedEventId = document.querySelector('option:checked').value
+
+  const eventData = await getPlayerEventRecord(playerId, selectedEventId)
+  
+  if (eventData) {
+    // success
+    document.querySelector('#noAccountFound').classList.add('d-none')
+    populateFieldsDivision(eventData)
+  } else {
+    // general failure
+    document.querySelector('#noAccountFound').classList.remove('d-none')
+    return
+  }
+
+  const eventBrackets = await getLeaderboardBrackets(playerId, selectedEventId)
+
+  if (eventBrackets) {
+    // success
+    document.querySelector('#eventLoadConnectionError').classList.add('d-none')
+    populateFieldsGlobal(eventBrackets)
+  } else {
+    // general failure
+    document.querySelector('#eventLoadConnectionError').classList.remove('d-none')
+  }
+}
+
+const getPlayerState = async(playerId) => {
+  return await fetch(`/api/player/${playerId}`)
+  .then((response) => {
+    if (response.status === 200) {
+      return true
+    } else {
+      document.querySelector('#noAccountFound').innerText = `No account ${playerId} found.`
+      return false
+    }
+  })
+  .catch((error) => {
+    console.error(error)
+    document.querySelector('#noAccountFound').innerText = 'Please check your internet connection'
+    return false
+  })
+}
+
+const getPlayerEventList = async(playerId) => {
+  return await fetch(`/api/list/${playerId}`)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((error) => {
+      console.error(error)
+      document.querySelector('option').innerText = 'Please check your internet connection'
+      return
+    })
+}
+
+const getPlayerEventRecord = async(playerId, eventId) => {
+  return await fetch(`/api/event/${eventId}/${playerId}`)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((error) => {
+      console.error(error)
+      document.querySelector('#eventLoadConnectionError').innerText = 'Please check your internet connection'
+      return
+    })
+}
+
+const getLeaderboardBrackets = async(playerId, eventId) => {
+  return await fetch(`/api/event/${eventId}/${playerId}/brackets`)
+    .then((response) => {
+      return response.json()
+    })
+    .catch((error) => {
+      console.error(error)
+      document.querySelector('#eventLoadConnectionError').innerText = 'Please check your internet connection'
+      return
+    })
+}
+
+const populateFieldsDivision = (data) => {
+  console.log(data)
+  console.log(data["player"]["globalPosition"])
+  console.log(data["global"]["count"])
+  document.querySelector('#playerName').innerText = getPlayerNameFromOrdinal(data["player"]["playerOrdinal"])
+  document.querySelector('#globalPosition').innerText = `${(data["player"]["globalPosition"]+1).toLocaleString()} / ${data["global"]["count"].toLocaleString()}`
+  document.querySelector('#globalPositionPercentile').innerText = `Top ${(data["player"]["globalPosition"] / (data["global"]["count"]-1) * 100).toFixed(2)}%`
+  document.querySelector('#joinDate').innerText = new Date(data["player"]["dateJoined"]).toLocaleString()
+  document.querySelector('#updateDate').innerText = new Date(data["player"]["dateUpdated"]).toLocaleString()
+  document.querySelector('#divisionId').innerText = data["player"]["divisionId"]
+  document.querySelector('#divisionType').innerText = getSpendingCategory(data["player"]["divisionId"])
+  document.querySelector('#leaderboardType').innerText = getLeaderboardType(data["player"]["divisionRoot"])
+  document.querySelector('#leaderboardId').innerText = data["event"]["eventGuid"]
+  
+  let tbody = document.querySelector('#divisionPlayers')
+  tbody.innerHTML = ''
+  for (let i in data["division"]["top"]) {
+    let divisionPlayer = document.createElement('tr')
+    if (data["division"]["top"][i]["ordinal"] === data["player"]["playerOrdinal"]) {
+      // bold the current player's line and add a trophy delta to the player above
+      divisionPlayer.classList.add('fw-bold')
+
+      if (i !== "0") {
+        let trophyDelta = data["division"]["top"][i-1]["trophies"] - data["division"]["top"][i]["trophies"] + 10
+
+        let moveUp = document.createElement('tr')
+        let moveUpCell = document.createElement('td')
+        moveUpCell.setAttribute('colspan', 3)
+        moveUpCell.classList.add('text-center')
+        moveUpCell.innerText = `▲ ${trophyDelta.toLocaleString()} trophies needed to move up ▲`
+
+        moveUp.appendChild(moveUpCell)
+        tbody.appendChild(moveUp)
+      }
+    }
+
+    let positionCell = document.createElement('td')
+    positionCell.innerText = parseInt(i) + 1
+
+    let nameCell = document.createElement('td')
+    nameCell.innerText = getPlayerNameFromOrdinal(data["division"]["top"][i]["ordinal"])
+
+    let trophyCell = document.createElement('td')
+    trophyCell.innerText = data["division"]["top"][i]["trophies"].toLocaleString()
+
+    divisionPlayer.appendChild(positionCell)
+    divisionPlayer.appendChild(nameCell)
+    divisionPlayer.appendChild(trophyCell)
+    
+    tbody.appendChild(divisionPlayer)
+  }
+}
+
+const populateFieldsGlobal = (data) => {
+  const bracketNames = [
+    'Champion',
+    'Top 5',
+    'Top 25',
+    'Top 100',
+    'Glorious',
+    'Elite',
+    'Prime',
+    'Grand',
+    'Advanced',
+    'Workforce'
+  ]
+  
+  let tbody = document.querySelector('#globalPositions')
+  tbody.innerHTML = ''
+
+  for (let i = 0; i < Object.keys(data["brackets"]).length; i++) {
+    let globalBracketLine = document.createElement('tr')
+
+    let bracketNameCell = document.createElement('td')
+    bracketNameCell.innerText = bracketNames[i]
+
+    let actualPositionInt
+    if (Object.keys(data["brackets"])[i] >= 1) {
+      actualPositionInt = (Object.keys(data["brackets"])[i])
+    } else {
+      actualPositionInt = Math.floor(data["totalPlayers"] * (Object.keys(data["brackets"])[i]))
+    }
+
+    let actualPositionCell = document.createElement('td')
+    actualPositionCell.innerText = actualPositionInt.toLocaleString()
+
+    let thresholdCell = document.createElement('td')
+    if (!Object.values(data["brackets"])[i]) {
+      // broken archived leaderboard
+      return
+    }
+    thresholdCell.innerText = Object.values(data["brackets"])[i].toLocaleString()
+
+    globalBracketLine.appendChild(bracketNameCell)
+    globalBracketLine.appendChild(actualPositionCell)
+    globalBracketLine.appendChild(thresholdCell)
+    
+    tbody.append(globalBracketLine)
+  }
+}
+
+document.querySelector('#formSubmitPlayFab').addEventListener('click', function() {
+  postFormPlayFab()
+})
+
+document.querySelector('#formSubmitEvent').addEventListener('click', function() {
+  postFormEvent()
+})
+
+if (localStorage.getItem('playerId')) {
+  let playerId = localStorage.getItem('playerId')
+  document.querySelector('#playFabQuery').value = playerId
+  document.querySelector('#playFabSetDefault').checked = true
+}
