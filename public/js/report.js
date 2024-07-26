90f5d88e0300e450b04a3f1a2f7d59354fb9c136
darrenrs <darrenrs0515@gmail.com>
2024-07-26 01:37:59 -0600

    Victory Lap event report

--- public/js/report.js
+++ public/js/report.js
@@ -73,12 +73,20 @@ const populateUserData = (data) => {
   let tbody = document.querySelector('#eventReport')
   const allExistingRows = document.querySelectorAll('tbody tr')
   
+  for (let i of allExistingRows) {
+    i.remove()
+  }
+  
   let cumulativeTrophies = 0
   let globalPositions = []
   let divPositions = []
-  
-  for (let i of allExistingRows) {
-    i.remove()
+
+  let eventCountWithCheaterFlag = 0
+  let bestPositionsByType = {
+    "weekend": null,
+    "mini": null,
+    "special": null,
+    "fusion": null
   }
 
   for (let i in data) {
@@ -98,7 +106,7 @@ const populateUserData = (data) => {
 
     let nameCell = document.createElement('td')
     if (!getEventDetails(data[i]["event"]["eventName"])) {
-      newCell.innerText = "Unknown event!"
+      nameCell.innerText = "Unknown event!"
     } else {
       nameCell.innerText = getEventDetails(data[i]["event"]["eventName"])["name"]
     }
@@ -143,6 +151,30 @@ const populateUserData = (data) => {
     if (data[i]["player"]["divisionPosition"] !== null) {
       divPositions.push(data[i]["player"]["divisionPosition"])
     }
+
+    // EVENT TYPE BEST OVERALL PERFORMANCE
+
+    // Skip currently active event or the "bugged" stone event
+    if ((new Date()).toISOString() < data[i]["event"]["endDate"] || data[i]["global"]["count"] < 1) {
+      continue
+    }
+
+    const eventType = getEventDetails(data[i]["event"]["eventName"])["eventType"]
+    const bracketScore = getBracketScoreFromPosition(data[i]["player"]["globalPosition"], data[i]["global"]["count"])
+
+    if (bestPositionsByType[eventType] === null || bracketScore > bestPositionsByType[eventType]["bracketScore"]) {
+      bestPositionsByType[eventType] = {
+        "eventId": data[i]["event"]["eventName"],
+        "eventName": getEventDetails(data[i]["event"]["eventName"])["name"],
+        "bracketScore": bracketScore,
+        "bracketName": getBracketNameFromId(bracketScore),
+        "dateRegistered": new Date(data[i]["player"]["dateUpdated"]).toGMTString().substring(5, 16)
+      }
+    }
+
+    if (data[i]["player"]["divisionRoot"] !== 'global') {
+      eventCountWithCheaterFlag ++
+    }
   }
 
   document.querySelector('table').classList.remove('d-none')
@@ -155,6 +187,168 @@ const populateUserData = (data) => {
     document.querySelector('#medianDivPos').innerHTML = getPositionHTMLFormat(divPositions.slice().sort((a, b) => a - b)[Math.floor(divPositions.length / 2)])
   } else {
     document.querySelector('#medianDivPos').innerText = '?'
+  }
+
+  if (eventCountWithCheaterFlag > 0) {
+    document.querySelector('#cheaterExclusion').classList.remove('d-none')
+    document.querySelector('#eventPositionApexByClass').parentElement.classList.add('d-none')
+    return
+  } else {
+    document.querySelector('#cheaterExclusion').classList.add('d-none')
+    document.querySelector('#eventPositionApexByClass').parentElement.classList.remove('d-none')
+  }
+
+  let missingEventTypes = 0
+  let bracketScoreSum = 0
+
+  for (let i of Object.keys(bestPositionsByType)) {
+    let eventPositionApexRow = document.createElement('tr')
+
+    let eventTypeCell = document.createElement('td')
+    eventTypeCell.innerText = i.substring(0,1).toUpperCase() + i.substring(1)
+
+    eventPositionApexRow.appendChild(eventTypeCell)
+
+    if (bestPositionsByType[i] === null) {
+      missingEventTypes ++
+
+      let zeroEntriesCell = document.createElement('td')
+      zeroEntriesCell.setAttribute('colspan', 5)
+      zeroEntriesCell.innerText = 'You haven\'t played this event type before!'
+      
+      eventPositionApexRow.appendChild(zeroEntriesCell)
+      document.querySelector('#eventPositionApexByClass').appendChild(eventPositionApexRow)
+      continue
+    }
+
+    let imageCell = document.createElement('td')
+    imageCell.style = 'padding-top: 0 !important; padding-bottom: 0 !important'
+
+    let image = document.createElement('img')
+    image.src = `img/adcom/icon/${bestPositionsByType[i]["eventId"]}.png`
+    image.style = 'height: 40px; width: 40px; background-color: rgba(0, 0, 0, 0) !important'
+    image.alt = '' // don't add a value to this or it'll look ugly
+    imageCell.appendChild(image)
+
+    let nameCell = document.createElement('td')
+    nameCell.innerText = bestPositionsByType[i]["eventName"]
+
+    let bracketCell = document.createElement('td')
+    bracketCell.innerText = bestPositionsByType[i]["bracketName"]
+
+    let bracketScoreCell = document.createElement('td')
+    bracketScoreCell.innerText = bestPositionsByType[i]["bracketScore"].toFixed(4)
+
+    let dateRegisteredCell = document.createElement('td')
+    dateRegisteredCell.innerText = bestPositionsByType[i]["dateRegistered"]
+
+    eventPositionApexRow.appendChild(imageCell)
+    eventPositionApexRow.appendChild(nameCell)
+    eventPositionApexRow.appendChild(bracketCell)
+    eventPositionApexRow.appendChild(bracketScoreCell)
+    eventPositionApexRow.appendChild(dateRegisteredCell)
+
+    document.querySelector('#eventPositionApexByClass').appendChild(eventPositionApexRow)
+
+    bracketScoreSum += bestPositionsByType[i]["bracketScore"]
+  }
+
+  if (missingEventTypes === 0) {
+    document.querySelector('#bracketScoreSum').innerText = `Bracket Score Sum: ${bracketScoreSum.toFixed(4)} (${getBracketScoreSumQualitativeDescriptor(bracketScoreSum)})`
+    document.querySelector('#bracketScoreSum').classList.remove('d-none')
+  } else {
+    document.querySelector('#bracketScoreSum').classList.add('d-none')
+  }
+}
+
+const getBracketScoreFromPosition = (position, total) => {
+  let baseScore
+  let minBracketPos
+  let maxBracketPos
+  
+  if (position < 1) {
+    return 10
+  } else if (position < 5) {
+    baseScore = 9
+    minBracketPos = 1
+    maxBracketPos = 5
+  } else if (position < 25) {
+    baseScore = 8
+    minBracketPos = 5
+    maxBracketPos = 25
+  } else if (position < 100) {
+    baseScore = 7
+    minBracketPos = 25
+    maxBracketPos = 100
+  } else if ((position+1)/total <= 0.01) {
+    baseScore = 6
+    minBracketPos = 100
+    maxBracketPos = Math.floor(total/100)
+  } else if ((position+1)/total <= 0.05) {
+    baseScore = 5
+    minBracketPos = Math.floor(total/100)
+    maxBracketPos = Math.floor(total/20)
+  } else if ((position+1)/total <= 0.1) {
+    baseScore = 4
+    minBracketPos = Math.floor(total/20)
+    maxBracketPos = Math.floor(total/10)
+  } else if ((position+1)/total <= 0.25) {
+    baseScore = 3
+    minBracketPos = Math.floor(total/10)
+    maxBracketPos = Math.floor(total/4)
+  } else if ((position+1)/total <= 0.5) {
+    baseScore = 2
+    minBracketPos = Math.floor(total/4)
+    maxBracketPos = Math.floor(total/2)
+  } else if ((position+1)/total <= 0.75) {
+    baseScore = 1
+    minBracketPos = Math.floor(total/2)
+    maxBracketPos = Math.floor(total/(4/3))
+  } else {
+    baseScore = 0
+    minBracketPos = Math.floor(total/(4/3))
+    maxBracketPos = total + 0
+  }
+
+  let actualPosition = position + 1 // starting at 1st, no 0
+  let percentageWithinBracket = (maxBracketPos - actualPosition) / (maxBracketPos - minBracketPos)
+
+  return percentageWithinBracket + baseScore
+}
+
+const getBracketNameFromId = (bracketScore) => {
+  const bracketNames = [
+    "Top 100%",
+    "Top 75%",
+    "Top 50%",
+    "Top 25%",
+    "Top 10%",
+    "Top 5%",
+    "Top 1%",
+    "Top 100",
+    "Top 25",
+    "Top 5",
+    "Champion"
+  ]
+
+  const bracketId = parseInt(Math.floor(bracketScore))
+
+  return bracketNames[bracketId]
+}
+
+const getBracketScoreSumQualitativeDescriptor = (bracketScoreSum) => {
+  if (bracketScoreSum >= 32) {
+    return "Ineffably high" // average 8.00 = 25th
+  } else if (bracketScoreSum >= 29) {
+    return "Extremely high" // average 7.25 = 75th = 0.1%
+  } else if (bracketScoreSum >= 27) {
+    return "Very high"      // average 6.75 = 200th = 0.2%
+  } else if (bracketScoreSum >= 25) {
+    return "Above average"  // average 6.25 = 500th = 0.7%
+  } else if (bracketScoreSum >= 20) {
+    return "Average"        // average 5.00 = 4000th = 5.0%
+  } else {
+    return "Below average"
   }
 }
 
